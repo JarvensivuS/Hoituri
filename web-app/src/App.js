aeb6b0a1f1385abd391043662a0415b89d2cbd39
Sampo Ikonen <sampo.ikonen1@gmail.com>
2025-02-13 09:44:12 +0200

    Medicine and Patient view fixes

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,32 +1,28 @@
 import React from "react";
 import MedicationSchedule from "./components/MedicationSchedule";
+import PatientView from "./components/PatientView";
 
 function App() {
   return (
-    <div>
-      <div>
-        <div class="container">
-          <h1>SeniiliSaver Web Interface</h1>
-          <div class="nav-placeholder">Navigation Placeholder</div>
-          <div class="content-placeholder">
-            <div class="card">
-              <h2>Patient Overview</h2>
-              <p>This section will show patient monitoring data.</p>
-            </div>
-            <div class="card">
-              <h2>Location Tracking</h2>
-              <p>This section will integrate with OpenStreetMap.</p>
-            </div>
-            <div class="card">
-              <MedicationSchedule />
-            </div>
-            <div class="card">
-              <h2>Alerts & Notifications</h2>
-              <p>
-                This section will display important alerts and notifications.
-              </p>
-            </div>
-          </div>
+    <div className="container">
+      <h1>Hoituri - Lääkärin näkymä</h1>
+      <div className="nav-placeholder">Navigation Placeholder</div>
+
+      {/* Cards layout using CSS Grid */}
+      <div className="content-placeholder">
+        <div className="card">
+          <PatientView />
+        </div>
+        <div className="card">
+          <h2>Location Tracking</h2>
+          <p>This section will integrate with OpenStreetMap.</p>
+        </div>
+        <div className="card">
+          <MedicationSchedule />
+        </div>
+        <div className="card">
+          <h2>Alerts & Notifications</h2>
+          <p>This section will display important alerts and notifications.</p>
         </div>
       </div>
     </div>
