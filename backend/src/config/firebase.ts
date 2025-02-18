2887b40785cb839c60846db613c1622d15c46716
SAMULI JÄRVENSIVU <samuli.jarvensivu@gmail.com>
2025-02-18 13:59:42 +0200

    changed db key name

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
-import * as serviceAccount from './hoituri-5a656-firebase-adminsdk-fbsvc-97e434287b.json';
+import * as serviceAccount from './DATABASE_SECRET_KEY.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
